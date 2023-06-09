cd66e673be2f45e05d1e6f0ef7b04840993ce326
JohnHoneyman <john.honeyman@98labs.com>
2023-06-09 16:47:58 +0800

    Incomplete lesson 27

--- 27-shader-patterns/src/shaders/test/fragment.glsl
+++ 27-shader-patterns/src/shaders/test/fragment.glsl
@@ -1,4 +1,93 @@
+varying vec2 vUv;
+
+// Pattern 23
+float random(vec2 st){
+    return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
+}
+
 void main()
 {
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+    // float strength = vUv.x; // Pattern 3
+    // float strength = vUv.y; // Pattern 4
+    // float strength = 1.0 - vUv.y; // Pattern 5
+    // float strength = vUv.y * 10.0; // Pattern 6
+    // float strength = mod(vUv.y * 10.0, 1.0); // Pattern 7
+
+    // // Pattern 8
+    // float strength = mod(vUv.y * 10.0, 1.0);
+    // strength = strength < 0.5 ? 0.0 : 1.0; 
+    // strength = step(0.5, strength); (another way)
+
+    // // Pattern 9
+    // float strength = mod(vUv.y * 10.0, 1.0);
+    // strength = step(0.8, strength); 
+
+    // // Pattern 10
+    // float strength = mod(vUv.x * 10.0, 1.0);
+    // strength = step(0.8, strength); 
+
+    // // Pattern 11
+    // float strength = step(0.8, mod(vUv.x * 10.0, 1.0)) + step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    // // Pattern 12
+    // float strength = step(0.8, mod(vUv.x * 10.0, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
+    
+    // // Pattern 13
+    // float strength = step(0.5, mod(vUv.x * 10.0, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
+    
+    // // Pattern 14
+    // float barX = step(0.5, mod(vUv.x * 10.0, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
+    // float barY = step(0.8, mod(vUv.x * 10.0, 1.0)) * step(0.5, mod(vUv.y * 10.0, 1.0));
+    // float strength = barX + barY;
+
+    // // Pattern 15;
+    // float barX = step(0.4, mod(vUv.x * 10.0, 1.0)) * step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
+    // float barY = step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0)) * step(0.4, mod(vUv.y * 10.0, 1.0));
+    // float strength = barX + barY;
+
+    // // Pattern 16
+    // float strength = abs(vUv.x - 0.5);
+
+    // // Pattern 17
+    // float strength = min(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+    
+    // // Pattern 18
+    // float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+
+    // // Pattern 19
+    // float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+
+    // // Pattern 20
+    // float square1 = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    // float square2 = 1.0 - step(0.25, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    // float strength = square1 * square2;
+
+    // // Pattern 21
+    // float strength = floor(vUv.x * 10.0) / 10.0;
+
+    // // Pattern 22
+    // float strength = (floor(vUv.x * 10.0) / 10.0) * (floor(vUv.y * 10.0) / 10.0);
+
+    // // Pattern 23
+    // float strength = random(vUv);
+
+    // // Pattern 24
+    // vec2 gridUv = vec2(
+    //     floor(vUv.x * 10.0) / 10.0,
+    //     floor(vUv.y * 10.0) / 10.0
+    // );
+    // float strength = random(gridUv);
+
+    // // Pattern 25
+    vec2 gridUv = vec2(
+        floor(vUv.x * 10.0) / 10.0,
+        floor(vUv.y * 10.0 + vUv.x * 5.0) / 10.0
+    );
+    float strength = random(gridUv);
+
+
+    // gl_FragColor = vec4(vUv, 1, 1.0); // Pattern 1
+    // gl_FragColor = vec4(vUv, 0, 1.0); // Pattern 2
+    // gl_FragColor = vec4(vUv, 0.5, 1.0); // Pattern 2.1
+    gl_FragColor = vec4(strength, strength, strength, 1.0); // Pattern 3+
 }
