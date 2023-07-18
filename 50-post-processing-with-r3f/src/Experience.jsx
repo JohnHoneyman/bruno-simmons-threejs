a463db8e4b670b137a89aa87c7e6232690a4d34f
JohnHoneyman <john.honeyman@98labs.com>
2023-07-18 20:03:14 +0800

    Finished lesson 50

--- 50-post-processing-with-r3f/src/Experience.jsx
+++ 50-post-processing-with-r3f/src/Experience.jsx
@@ -1,31 +1,143 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+// https://github.com/pmndrs/postprocessing
+// https://pmndrs.github.io/postprocessing/public/docs/
+// https://pmndrs.github.io/postprocessing/public/demo/
+// https://github.com/pmndrs/reactpostprocessing
+// https://github.com/pmndrs/react-postprocessing/blob/master/api.md
+// https://docs.pmnd.rs/react-postprocessing/introduction
 
-export default function Experience()
-{
-    return <>
+import { useRef } from "react";
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import {
+  Glitch,
+  Vignette,
+  EffectComposer,
+  Noise,
+  Bloom,
+  DepthOfField,
+  SSR,
+} from "@react-three/postprocessing";
+import { GlitchMode, BlendFunction } from "postprocessing";
+import { useControls } from "leva";
 
-        <Perf position="top-left" />
+import Drunk from "./Drunk";
 
-        <OrbitControls makeDefault />
+// TODO: CONTINUE AT 1:11:37
+export default function Experience() {
+  const drunkRef = useRef();
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+  const ssrProps = useControls("SSR Effect", {
+    temporalResolve: true,
+    STRETCH_MISSED_RAYS: true,
+    USE_MRT: true,
+    USE_NORMALMAP: true,
+    USE_ROUGHNESSMAP: true,
+    ENABLE_JITTERING: true,
+    ENABLE_BLUR: true,
+    temporalResolveMix: { value: 0.9, min: 0, max: 1 },
+    temporalResolveCorrectionMix: { value: 0.25, min: 0, max: 1 },
+    maxSamples: { value: 0, min: 0, max: 1 },
+    resolutionScale: { value: 1, min: 0, max: 1 },
+    blurMix: { value: 0.5, min: 0, max: 1 },
+    blurKernelSize: { value: 8, min: 0, max: 8 },
+    blurSharpness: { value: 0.5, min: 0, max: 1 },
+    rayStep: { value: 0.3, min: 0, max: 1 },
+    intensity: { value: 1, min: 0, max: 5 },
+    maxRoughness: { value: 0.1, min: 0, max: 1 },
+    jitter: { value: 0.7, min: 0, max: 5 },
+    jitterSpread: { value: 0.45, min: 0, max: 1 },
+    jitterRough: { value: 0.1, min: 0, max: 1 },
+    roughnessFadeOut: { value: 1, min: 0, max: 1 },
+    rayFadeOut: { value: 0, min: 0, max: 1 },
+    MAX_STEPS: { value: 20, min: 0, max: 20 },
+    NUM_BINARY_SEARCH_STEPS: { value: 5, min: 0, max: 10 },
+    maxDepthDifference: { value: 3, min: 0, max: 10 },
+    maxDepth: { value: 1, min: 0, max: 1 },
+    thickness: { value: 10, min: 0, max: 10 },
+    ior: { value: 1.45, min: 0, max: 2 },
+  });
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+  const drunkProps = useControls("Drunk Effect", {
+    frequency: { value: 2, min: 1, max: 20 },
+    amplitude: { value: 0.1, min: 0, max: 1 },
+  });
 
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+  return (
+    <>
+      <color args={["#FFFFFF"]} attach="background" />
+      {/* <color args={["#000000"]} attach="background" /> */}
 
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+      <EffectComposer>
+        {/* <Vignette
+          offset={0.3}
+          darkness={0.9}
+          blendFunction={BlendFunction.NORMAL}
+        /> */}
+        {/* <Glitch
+          delay={[0.5, 1]}
+          duration={[0.1, 0.3]}
+          strength={[0.02, 0.04]}
+          mode={GlitchMode.CONSTANT_WILD}
+        /> */}
+        {/* <Noise blendFunction={BlendFunction.SOFT_LIGHT} premultiply /> */}
+        {/* <Noise blendFunction={BlendFunction.OVERLAY} premultiply /> */}
+        {/* <Noise blendFunction={BlendFunction.SCREEN} premultiply /> */}
+        {/* <Noise blendFunction={BlendFunction.AVERAGE} premultiply /> */}
+        {/* <Bloom
+          // must set tonemapping to false on mesh color value needs to be greater than 1 or emmissive
+          mipmapBlur
+          intensity={0.1}
+          luminanceThreshold={0}
+          luminanceSmoothing={0.1}
+          height={300}
+        /> */}
+        {/* <DepthOfField
+          focusDistance={0.025}
+          focalLength={0.025}
+          bokehScale={6}
+        /> */}
+        {/* <SSR {...ssrProps} /> */}
+        <Drunk
+          ref={drunkRef}
+          {...drunkProps}
+          blendFunction={BlendFunction.DARKEN}
+        />
+      </EffectComposer>
 
+      <Perf position="top-left" />
+
+      <OrbitControls makeDefault />
+
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
+      <mesh castShadow position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <mesh castShadow position-x={2} scale={1.5}>
+        <boxGeometry />
+        {/* <meshStandardMaterial
+          // color={[5, 2, 1]}
+          color="white"
+          emissive="orange"
+          emissiveIntensity={1}
+          toneMapped={false}
+        /> */}
+        {/* <meshBasicMaterial color={[1.5, 1, 4]} toneMapped={false} /> */}
+        <meshBasicMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh
+        receiveShadow
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}
+      >
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" metalness={0} roughness={0} />
+      </mesh>
     </>
+  );
 }
